27f037cc8f7069c88fb37edfc08858f67c0de83c
taeheon <8269apk@naver.com>
2021-09-27 20:33:06 +0900

    z

--- server/server.js
+++ server/server.js
@@ -5,8 +5,30 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res)=> {
-    res.send('ㅋㅋ');
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host : "localhost",
+    user : "root", //mysql의 id
+    password : "xogjs0504", //mysql의 password
+    database : "reactin", //사용할 데이터베이스
+});
+
+connection.connect();
+var AK
+connection.query("SELECT * FROM person", function (error, result, fields) {
+    if(error) throw error;
+    AK = result[0].Text;
+    console.log(typeof(AK))
+    if(AK == 'Love you'){
+        console.log('Hello')
+    }
+    connection.end();
+});
+
+
+app.get('/api/host', (req, res)=> {
+    res.send({ host : 'Taeheon' , kate : AK });
 })
 
 app.listen(PORT, () => {
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      aa : '',
+      bb : '',
+    }
+  }
+
+  componentDidMount() {
+    this._getHost();
+  }
+
+  _getHost = async() => {
+    const res = await axios.get('/api/host');
+    console.log(res.data);
+    this.setState({ aa : res.data.host })
+    this.setState({ bb : res.data.kate })
+  }
+
+  render() {
+    return(
+      <div className='App'>
+        <h3> Welcome to <u> {this.state.aa} </u> Blog! 
+        {this.state.bb} </h3>
+      </div>
+    )
+  }
 }
 
 export default App;
+
